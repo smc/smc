b88f2e7e4e2a93675f0b354b4c9745389c4693ae
Rajeesh K Nambiar <rajeeshknambiar@gmail.com>
2009-01-14 20:05:39 +0530

    Add APIs 'word2ASCII' and 'word2Unicode'

--- payyans/payyans/__init__.py
+++ payyans/payyans/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-# Copyright 2008 Santhosh Thottingal <santhosh.thottingal@gmail.com>, Nishan Naseer <nishan.naseer@gmail.com>
+# Copyright 2008 Santhosh Thottingal <santhosh.thottingal@gmail.com>, Nishan Naseer <nishan.naseer@gmail.com>, Rajeesh K Nambiar <rajeeshknambiar@gmail.com>
 # http://www.smc.org.in
 """Payyans is a python program to convert the data written for ascii fonts in ascii format to the Unicode format"""
 
-__AUTHORS__ = [ ("Santhosh Thottingal", "santhosh.thottingal@gmail.com", "Nishan Naseer", "nishan.naseer@gmail.com")]
+__AUTHORS__ = [ ("Santhosh Thottingal", "santhosh.thottingal@gmail.com", "Nishan Naseer", "nishan.naseer@gmail.com", "Rajeesh K Nambiar", "rajeeshknambiar@gmail.com")]
 
 from payyan import Payyan
 import os
@@ -18,8 +18,11 @@ class Payyans:
 		self.payyan.input_filename=input_file 
 		self.payyan.output_filename=output_file
 		self.payyan.mapping_filename=mapping_file
-		
-		
+	
+	def word2ASCII(self, unicode_str):
+		return self.payyan.word2ASCII(unicode_str)
+	def word2Unicode(self, ascii_str):
+		return self.payyan.word2ASCII(ascii_str)
 	def ascii2unicode(self):
 		return self.payyan.Ascii2Uni()
 	def unicode2ascii(self):
