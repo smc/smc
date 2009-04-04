7bbd521108e0b0b095bd761d8d3db27195707999
aashiks <aashiks@gmail.com>
2009-04-04 11:58:54 +0530

    Install.sh contained some code for invoking the plasmoidviewer. This is not necessary.

--- fortune-ml/plasmoid/install.sh
+++ fortune-ml/plasmoid/install.sh
@@ -3,5 +3,7 @@ rm ../FortuneCookie.zip
 zip -r ../FortuneCookie.zip .
 plasmapkg -r fortunecookie
 plasmapkg -i ../FortuneCookie.zip
-echo "invoking Plasmoid Viewer..."
-plasmoidviewer fortunecookie
+
+#--- Uncomment the following lines to see the plasmoid in a test cotainer.
+#echo "invoking Plasmoid Viewer..."
+#plasmoidviewer fortunecookie
