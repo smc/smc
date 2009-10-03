e4c73b927e00bfaf5d797fbfadae191f6375cac0
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2009-10-03 12:01:24 +0530

    Adding inkscape hyphenation extension

---
+++ hyphenation/inkscape/extensions/text_hyphenate.py
@@ -0,0 +1,9 @@
+import chardataeffect, inkex, string
+from hyphenator import Hyphenator
+class C(chardataeffect.CharDataEffect):
+  def process_chardata(self,text, line=False, par=False):
+    # insert softhyphens
+    return Hyphenator().hyphenate(text, u'\u00AD')
+	
+c = C()
+c.affect()
