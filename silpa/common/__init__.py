16868ab2b4d0e74e9aa4384ac7f7114bf3e5b400
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2010-06-10 21:21:33 +0530

    removing the outdated silpa folder from git.

--- silpa/common/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from silpamodule import SilpaModule ,ServiceMethod
-from silparequest import SilpaRequest
-from modulemanager import ModuleManager
-from json import loads, dumps, JSONEncodeException, JSONDecodeException
-from charmap import *
-from fcgi import WSGIServer
-
-
