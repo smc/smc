70c237d923abc3a01bfd5c6451e3d5d98fc0a15c
Rajeesh K Nambiar <rajeeshknambiar@gmail.com>
2010-08-05 01:27:40 +0530

    Remove input-methods and hyphenation directories

--- hyphenation/inkscape/extensions/text_hyphenate.py
+++
@@ -1,9 +0,0 @@
-import chardataeffect, inkex, string
-from hyphenator import Hyphenator
-class C(chardataeffect.CharDataEffect):
-  def process_chardata(self,text, line=False, par=False):
-    # insert softhyphens
-    return Hyphenator().hyphenate(text, u'\u00AD')
-	
-c = C()
-c.affect()
--- input-methods/m17n/swanalekha/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cp *.mim /usr/share/m17n/
-cp *.png /usr/share/m17n/icons
-
